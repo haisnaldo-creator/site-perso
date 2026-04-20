5e6da37b95ae15e6fa13e0e120da9670849a666f
haisnaldo-creator <haisnaldo@gmail.com>
2026-04-20 16:13:25 +0200

    Update Comments.jsx

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -23,7 +23,7 @@ export default function Comments({ slug }) {
     }
     useEffect(() => {
         load();
-    }, [slug]);
+    }, []);
 
     async function submit(e) {
         e.preventDefault();
